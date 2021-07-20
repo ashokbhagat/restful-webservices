431f221630fb9ec40f6254d19a5a956b0cd45d88
Ashok Bhagat <ashoka.bhagat@gmail.com>
2021-07-20 17:55:57 +0530

    Support for internationlization

--- restful-webservices/src/main/java/com/ashok/practice/rest/webservices/controller/HelloWorldController.java
+++ restful-webservices/src/main/java/com/ashok/practice/rest/webservices/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.ashok.practice.rest.webservices.controller;
 
 import com.ashok.practice.rest.webservices.domain.HelloWorldBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path = "/hello-world")
     public String helloWorld() {
@@ -22,9 +30,10 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(name);
     }
-    @GetMapping(path="/hello-world/internationlized")
-    public String internationalized(){
-        return "";
+
+    @GetMapping(path="/hello-world-internationlized")
+    public String internationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 
 }
