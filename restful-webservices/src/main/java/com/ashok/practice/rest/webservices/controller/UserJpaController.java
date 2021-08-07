0ce56fcc363566211840755dda1c69ecf567d8a2
Ashok Bhagat <ashoka.bhagat@gmail.com>
2021-08-07 23:34:31 +0530

    JPA

--- restful-webservices/src/main/java/com/ashok/practice/rest/webservices/controller/UserJpaController.java
+++ restful-webservices/src/main/java/com/ashok/practice/rest/webservices/controller/UserJpaController.java
@@ -37,25 +37,5 @@ public class UserJpaController {
             throw new UserNotFoundException(id + " user is not found");
         return user.get();
     }
-
-    @PostMapping("/jpa/users")
-    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
-        User savedUser = userDAO.save(user);
-        URI location = ServletUriComponentsBuilder
-                .fromCurrentRequest()
-                .path("/{id}")
-                .buildAndExpand(savedUser.getId())
-                .toUri();
-        return ResponseEntity.created(location).build();
-    }
-
-    @DeleteMapping("/jpa/users/{id}")
-    public void deleteById(@PathVariable int id) throws UserNotSupportedException {
-        User user = userDAO.deleteById(id);
-
-        if (user == null) {
-            throw new UserNotSupportedException("id : " + id + " is not found, can not delete");
-        }
-    }
-
+   
 }
